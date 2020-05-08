d89d4372362b5abd020b6374e090190d91fde5d6
katie-simmons <katie@katie.codes>
2020-05-07 21:05:38 -0500

    Test for getting pods since docs don't cover getting deployments

--- test_k8s_wrangler.py
+++ test_k8s_wrangler.py
@@ -4,6 +4,9 @@ from k8s_wrangler import KubernetesWrangler
 class TestK8sWrangler:
     def setup(self):
         self.kw = KubernetesWrangler()
+    
+    def test_get_pods(self):
+        assert self.kw.get_pods() == {"test-pod": 1.0}
 
     def test_get_deployments(self):
-        assert self.kw.get_deployments() == {"test-pod": 1.0}
+        assert self.kw.get_deployments() == {}
