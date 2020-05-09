00f8af33cf3b82a02ab7f5ad64e230060858426e
katie-simmons <katie@katie.codes>
2020-05-09 16:57:45 -0500

    Update the method to get pods to return a dict of each pods container names and versions

--- k8s_wrangler.py
+++ k8s_wrangler.py
@@ -1,7 +1,5 @@
 from kubernetes import client, config
 
-nodes = ['192.168.64.3']
-
 class KubernetesWrangler:
     def __init__(self):
         self.deployments = {}
@@ -11,15 +9,24 @@ class KubernetesWrangler:
         v1 = client.CoreV1Api()
         return v1
 
-    def get_pods(self):
-        pods = []
-        conn = self.connect()
-        ret = conn.list_pod_for_all_namespaces(watch=False)
-        for item in ret.items:
-            if item.status.pod_ip not in nodes:
-                pods.append(item.status.pod_ip)
-        return pods
+    def get_pods(self, namespace="default"):
+        try:
+          pods = {}
+          conn = self.connect()
+          ret = conn.list_namespaced_pod(namespace, timeout_seconds=90, limit=25)
+          for item in ret.items:
+            container_vals = item.spec.containers[0].image.split(':')
+            container_name = container_vals[0].split('/')[1]
+            container_version = container_vals[1].strip("'")
+            pods[container_name] = container_version
+          return pods
+        except Exception as e:
+          print({'Error': e})
 
     def get_deployments(self):
         # conn = self.connect()
         return self.deployments
+
+if __name__ == "__main__":
+  kw = KubernetesWrangler()
+  kw.get_pods()
