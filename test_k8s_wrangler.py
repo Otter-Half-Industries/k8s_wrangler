742778690ce37d85244cbdbcd47c598b6a07be59
rockhowse <rockhowse@gmail.com>
2020-05-09 16:00:13 -0500

    Getting pods should return a dict with the container name and version

--- test_k8s_wrangler.py
+++ test_k8s_wrangler.py
@@ -4,9 +4,9 @@ from k8s_wrangler import KubernetesWrangler
 class TestK8sWrangler:
     def setup(self):
         self.kw = KubernetesWrangler()
-    
+
     def test_get_pods(self):
-        assert self.kw.get_pods() == ['172.17.0.2', '172.17.0.3']
+        assert self.kw.get_pods() == {'k8s.gcr.io/echoserver': 1.4}
 
     def test_get_deployments(self):
         assert self.kw.get_deployments() == {}
